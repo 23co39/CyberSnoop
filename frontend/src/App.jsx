d3aa57ca674d213f344b0441aa7202f5f8baf5da
owais khan <23co39@aiktc.ac.in>
2025-07-26 14:26:37 +0530

    Add backend and frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="justify-center text-center py-3 bg-slate-800 text-3xl font-bold underline">
+      CyberSnoop
+     </h1>
+    </>
+  )
+}
+
+export default App
